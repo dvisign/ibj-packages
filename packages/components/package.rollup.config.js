e5adf1158e5e9c2bf2fc5fffb4f4ba26ac87c0c9
dvisign <jmch0922@gmail.com>
2024-03-07 17:05:04 +0900

    컴포넌트 개별분리

--- packages/components/package.rollup.config.js
+++ packages/components/package.rollup.config.js
@@ -1,40 +1,28 @@
-import path from "path"
-import { fileURLToPath } from "url"
 import { terser } from "rollup-plugin-terser";
 import nodePolyfills from "rollup-plugin-node-polyfills";
 import sucrase from "@rollup/plugin-sucrase";
 import typescript from "@rollup/plugin-typescript";
 import external from "rollup-plugin-peer-deps-external";
-import alias from "@rollup/plugin-alias";
 import commonjs from "@rollup/plugin-commonjs"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const rollupConfig = [
   {
     input: "packages/components/index.tsx",
-    output: [
-      {
-        file: "dist/components/index.js",
-        format: "umd",
-        name: "ibjComponents",
-        sourcemap: process.env.NODE_ENV === "production" ? true : false,
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
+    output: {
+      // file: "dist/components/index.js",
+      dir: "dist/components",
+      format: "es",
+      name: "ibjComponents",
+      sourcemap: process.env.NODE_ENV === "production" ? true : false,
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
       },
-      {
-        file: "dist/components/index.esm.js",
-        format: "es",
-        sourcemap: process.env.NODE_ENV === "production" ? true : false,
-      },
-    ],
+    },
     plugins: [
       external(),
       commonjs(),
-      typescript({ tsconfig: "./packages/components/tsconfig.json"}),
+      typescript({ tsconfig: "./packages/components/tsconfig.json", outDir: "./dist/components", outputToFilesystem: true }),
       sucrase({
         exclude: ["node_modules/**"],
         transforms: ["typescript", "jsx"],
