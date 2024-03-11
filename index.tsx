7c34128eb920586f2b37f4d117ecc25ed9390ac7
booldook <booldook@gmail.com>
2024-03-11 17:41:19 +0900

    다중 export

--- index.tsx
+++ index.tsx
@@ -1,25 +1,29 @@
-import {
-  baseConfig,
-  nextConfig,
-  reactLibraryConfig,
-  baseConfigJson,
-  nextConfigJson,
-  reactLibraryConfigJson,
-} from "./packages/typescript-config"
+// import {
+//   baseConfig,
+//   nextConfig,
+//   reactLibraryConfig,
+//   baseConfigJson,
+//   nextConfigJson,
+//   reactLibraryConfigJson,
+// } from "./packages/typescript-config"
+//
+// import { libraryLintConfig, nextLintConfig } from "./packages/eslint-config"
+// import { Buttons, TextInput, TextLabel } from "./packages/components"
 
-import { libraryLintConfig, nextLintConfig } from "./packages/eslint-config"
-import { Buttons, TextInput, TextLabel } from "./packages/components"
+// export {
+//   Buttons,
+//   TextInput,
+//   TextLabel,
+//   baseConfig,
+//   nextConfig,
+//   reactLibraryConfig,
+//   baseConfigJson,
+//   nextConfigJson,
+//   reactLibraryConfigJson,
+//   libraryLintConfig,
+//   nextLintConfig,
+// }
 
-export {
-  Buttons,
-  TextInput,
-  TextLabel,
-  baseConfig,
-  nextConfig,
-  reactLibraryConfig,
-  baseConfigJson,
-  nextConfigJson,
-  reactLibraryConfigJson,
-  libraryLintConfig,
-  nextLintConfig,
-}
+export * from "./packages/components"
+export * from "./packages/eslint-config"
+export * from "./packages/typescript-config"
